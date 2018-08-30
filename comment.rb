f20d927edd32869b2409b7d893c045dbbb29cd2f
王昱斌 <wangyubin@wallstreetcn.com>
2018-08-30 20:35:35 +0800

    Site updated: 2018-08-30 20:35:34

--- comment.rb
+++ comment.rb
@@ -41,6 +41,7 @@ end
 urls.each_with_index do |url, index|
 
   if commenteds.include?("#{url}\n") == false
+    puts "-------#{url}"
     url_key = Digest::MD5.hexdigest(URI.parse(url).path)
     response = conn.get "/search/issues?q=label:#{url_key}+state:open+repo:#{username}/#{repo_name}"
 
@@ -50,7 +51,7 @@ urls.each_with_index do |url, index|
       puts "正在创建: #{url}"
       title = open(url).read.scan(/<title>(.*?)<\/title>/).first.first.force_encoding('UTF-8')
       response = conn.post("/repos/#{username}/#{repo_name}/issues") do |req|
-      req.body = { body: url, labels: [kind, url[0,48]], title: title }.to_json
+      req.body = { body: url, labels: [kind, url], title: title }.to_json
       end
       if JSON.parse(response.body)['number'] > 0
         `echo #{url} >> .commenteds`
